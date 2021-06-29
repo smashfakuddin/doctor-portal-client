f235701430231f0f57cc8e44c67ff74901e52862
smashfakuddin <smashfakuddin.anthro.cou@gmail.com>
2021-06-29 23:23:26 +0600

    first commit

---
+++ src/components/Home/HeaderMain/HeaderMain.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import chair from '../../../img/chair.png';
+import './HeaderMain.css';
+
+const HeaderMain = () => {
+    return (
+        <main style={{ height: '550px' }} className='row g-3 d-flex align-items-center  '>
+            <div className="col-md-4 offset-md-1 offset-1 col-8 ">
+                <h1>Your New Smile <br /> Start Here</h1>
+                <p className='text-justify'>Lorem ipsum, dolor sit amet consectetur
+                    adipisicing elit. Deleniti porro unde voluptatem cum iure perspiciatis
+                    quos doloremque. Lorem ipsum dolor sit amet consectetur adipisicing
+                    elit. Facilis repellat dolorem eius?</p>
+                <button className="custom-button">Get Appointment</button>
+            </div>
+            <div className="col-md-6 ms-3  offset-2 col-10 ">
+                <img src={chair} alt="" width='700' className='img-fluid' />
+            </div>
+        </main>
+
+    );
+};
+
+export default HeaderMain;
---
+++ src/components/Home/Navbar/Navbar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className="row bg-light">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light col-11 mx-auto">
+                <div className="container-fluid">
+                    <a className="navbar-brand fw-bold fs-4" href="/">Doctor Portal</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="/home">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link " href="/about">About</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link " href="/dental-services">Dental Services</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link  " href="/review">Reviews</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link  " href="/blog">Blog</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link  " href="/contact">Contacts</a>
+                            </li>
+                        </ul>
+
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
